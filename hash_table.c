ebb06152e4834a34ef6bd9c2a2487f6a2796d963
ZJF1834 <270829147@qq.com>
2018-08-01 16:05:18 +0800

    哈希表

--- hash_table.c
+++ hash_table.c
@@ -5,24 +5,24 @@
 
 typedef struct Data {
 	char *str;
-	// int flag;
-	struct Data, *next;
+	struct Data *next;
 } Data;
 
 typedef struct HashTable {
 	Data *data;
 	int size;
+	int cnt;
 	int (*hash_func)(const char *);
 
 } HashTable;
 
-int APHash(const cahr *str) {
+int APHash(const char *str) {
 	int hash = 0;
 	for (int i = 0; str[i]; i++) {
 		if (i & 1) {
 			hash ^= (hash << 7) ^ str[i] ^ (hash >> 3);
 		} else {
-			hash ^= (~((hash << 11) ^ str[i] ^ (hash >> 5)))
+			hash ^= (~((hash << 11) ^ str[i] ^ (hash >> 5)));
 		}
 	}
 	return hash & 0x7fffffff;
@@ -36,53 +36,7 @@ int BKDRHash(const char *str) {
 	return hash & 0x7fffffff;
 }
 
-int __APHash(Data *data) {
-	return APHash(data->str);
-}
-
-int __BKDRHash(Data *data) {
-	return BKDRHash(data->str);
-}
-
-int __ZobristHash(Data *data) {
-	return ZobrisHash(data->str);
-}
-
-HashTable *init(int size, int (*func)(const cahr *)) {
-	HashTable *p = (HashTable *)malloc(sizeof(HashTable));
-	p->data = (Data *)calloc(sizeof(Data), size);
-	p->size = size;
-	p->hash_func = func;
-	return p;
-}
-
-void clear(HashTable *h) {
-	if (h == NULL) return ;
-	free(h->data);
-	free(h);
-	return ;
-}
-
-int query(HashTable *h, const char *str) {
-	int ind = h->hash_func(str) % h->size;
-	Data *p  = &h->data[ind].next;
-	while (p) {
-		if (strcmp(p->str, str) == 0) return -1;
-		p = p->next;
-	}
-	h->cnt += 1;
-	return ind;
-}
-
-void insert(HashTable *h, const char * str) {
-	int ind = query(h, str);
-	if (ind == -1) return ;
-	Data *data = (Data *)malloc(sizeof(Data));
-	data->str = strdup(str);
-	data->next = h->data[ind].next;
-	h->data[ind].next = data;
-	return ;
-}
+int zobrist_table[50][256] = {0};
 
 void init_zobrist_table() {
 	for (int i = 0; i < 50; i++) {
@@ -93,9 +47,7 @@ void init_zobrist_table() {
 	return ;
 }
 
-int zobrist_table[50][256] = {0};
-
-int ZobrisHash(const char *str){
+int ZobristHash(const char *str){
 	static int flag = 1;
 	if (flag) {
 		flag = 0;
@@ -116,12 +68,50 @@ int MyHash(const char *str) {
 	return hash & 0x7fffffff;
 }
 
+HashTable *init(int size, int (*func)(const char *)) {
+	HashTable *p = (HashTable *)malloc(sizeof(HashTable));
+	p->data = (Data *)calloc(sizeof(Data), size);
+	p->size = size;
+	p->hash_func = func;
+	p->cnt = 0;
+	return p;
+}
+
+void clear(HashTable *h) {
+	if (h == NULL) return ;
+	free(h->data);
+	free(h);
+	return ;
+}
+
+int query(HashTable *h, const char *str) {
+	int ind = h->hash_func(str) % h->size;
+	Data *p  = h->data[ind].next;
+	while (p) {
+		if (strcmp(p->str, str) == 0) return -1;
+		p = p->next;
+		h->cnt += 1;
+	}
+	return ind;
+}
+
+void insert(HashTable *h, const char * str) {
+	int ind = query(h, str);
+	if (ind == -1) return ;
+	Data *data = (Data *)malloc(sizeof(Data));
+	data->str = strdup(str);
+	data->next = h->data[ind].next;
+	h->data[ind].next = data;
+	return ;
+}
+
 int main() {
-	HashTable *apHashTable = init(100, APHash);
-	HashTable *bkdrHashTable = init(100, BKDRHash);
-	HashTable *zobristHashTable = init(100, ZobrisHash);
-	HashTable *myHashTable = init(100, MyHash);
- 	#define MAX_TEST_CNT 1000000
+	#define HASH_TABLE_SIZE 100000
+	HashTable *apHashTable = init(HASH_TABLE_SIZE, APHash);
+	HashTable *bkdrHashTable = init(HASH_TABLE_SIZE, BKDRHash);
+	HashTable *zobristHashTable = init(HASH_TABLE_SIZE, ZobristHash);
+	HashTable *myHashTable = init(HASH_TABLE_SIZE, MyHash);
+ 	#define MAX_TEST_CNT 100000
  	for (int i = 0; i < MAX_TEST_CNT; i++) {
  		char str[7] = {0};
  		for (int j = 0; j < 6; j++) {
@@ -130,12 +120,14 @@ int main() {
  		str[6] = 0;
  		insert(apHashTable, str);
  		insert(bkdrHashTable, str);
- 		insert(zobrist_table, str);
+ 		insert(zobristHashTable, str);
+ 		insert(myHashTable, str);
  	}
- 	printf("APHash(%d) = %d\n", MAX_TEST_CNT, apHashTable->cnt);;
- 	printf("BKDRHash(%d) = %d\n", MAX_TEST_CNT, bkdrHashTable->cnt);;
- 	printf("ZobrisHash(%d) = %d\n", MAX_TEST_CNT, zobrisHashHashTable->cnt);;
- 	printf("MyHash(%d) = %d\n", MAX_TEST_CNT, MyHashTable->cnt);;
+ 	printf("APHash(%d) = %d\n", MAX_TEST_CNT, apHashTable->cnt);
+ 	printf("BKDRHash(%d) = %d\n", MAX_TEST_CNT, bkdrHashTable->cnt);
+ 	printf("ZobrisHash(%d) = %d\n", MAX_TEST_CNT, zobristHashTable->cnt);
+ 	printf("MyHash(%d) = %d\n", MAX_TEST_CNT, myHashTable->cnt);
  	#undef MAX_TEST_CNT
+ 	#undef HASH_TABLE_SIZE
 	return 0;
 }
