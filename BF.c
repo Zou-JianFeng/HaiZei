0374d79dffd679e985be1914e76151627ae6c078
ZJF1834 <270829147@qq.com>
2018-08-03 20:29:12 +0800

    BF

--- BF.c
+++ BF.c
@@ -16,31 +16,9 @@ int BF(const char *text, const char *pattern) {
 	return 0;
 }
 
-int KMP(const char *text, const char *pattern) {
-	int len1 = strlen(text);
-	int len2 = strlen(pattern);
-	int *next = (int *)malloc(sizeof(int) * len2);
-	int j = -1;
-	int cnt = 0;
-	next[0] = -1;
-	for (int i = 1; i < len2; ++i) {
-		while (j != -1 && pattern[j + 1] != pattern[i]) j = next[j];
-		if (pattern[j + 1] == pattern[j]) ++j;
-		next[i] = j;
-	}
-	j = -1;
-	for (int i = 0; text[i]; ++i) {
-		while (j != -1 && pattern[j + 1] != text[i]) j = next[j];
-		if (pattern[j + 1] == text[i]) ++j;
-		if (pattern[j + 1] == 0) return 1;
-	}
-	return 0;
-}
-
 int main() {
 	char str1[] = "hello world";
 	printf("BF(%s, %s) = %d\n", str1, "wor", BF(str1, "wor"));
-	printf("KMP(%s, %s) = %d\n", str1, "wor", KMP(str1, "wor"));
 
 	return 0;
 }
