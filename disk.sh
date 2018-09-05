98cb03649091af93219d56033199cf720c5d186b
Zou-JianFeng <40564676+Zou-JianFeng@users.noreply.github.com>
2018-09-05 22:03:03 +0800

    disk。sh

--- disk.sh
+++ disk.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-NowTime=`date +"%Y-%m-%d__%H:%M:%S"`
-
-# df -h | awk '{if($1 ~ /\/dev\//) {printf("%s %s\n",$2,$3)}}'
-Alldisk=(` df -m | awk -v sum=0 '{if($1 ~ /\/dev\//) {sum=sum+$2}} END {print sum}' `)
-
-AllAva=(` df -m | awk -v sum=0 '{if($1 ~ /\/dev\//) {sum=sum+$4}} END {print sum}' `)
-
-Used=$[100 - $AllAva *100 /$Alldisk ]
-
-echo "$NowTime 0 disk $Alldisk $AllAva ${Used}%"  #>> $DiskLog
-
-df -m | awk -v d=${NowTime} '{if($1 ~ /\/dev\//){print "'$NowTime'","1",$6,$2,$4,$5}}'  #>>$DiskLog
+nowtime=`date +%Y-%m-%d__%H:%M:%S`
+allSize=(`df -hl | awk -v sum=0 '{if($1 ~ /\/dev\//) {sum=$2}} END{print sum}'`)
+allAva=(`df -hl | awk -v sum=0 '{if($1 ~ /\/dev\//) {sum=$4}} END{print sum}'`)
+Used=(`df -hl | awk -v sum=0 '{if($1 ~ /\/dev\//) {sum=sum+$5}} END{print sum}'`)
+echo $nowtime 0 总量 $allSize 剩余量$allAva 当前占用百分比 ${Used}%
+df -hl | awk -v d=${NowTime} '{if($1 ~ /\/dev\//){print "'$nowtime' "1, $2, $4, $5}}'
